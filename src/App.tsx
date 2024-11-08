eb76f545e04f32e54f1de1bfa4294055428eb01c
developer-jyyun <developer.jyyun@gmail.com>
2024-11-08 18:25:05 +0900

    Feat: 청첩장 데이터 불러오기 및 로딩,에러 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
 import styles from './App.module.scss'
+import FullScreenMessage from './components/common/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 function App() {
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못함')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.error('Error:', e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />
+  }
+  if (error) {
+    return (
+      <FullScreenMessage
+        type="error"
+        text="에러가 발생했어요! 잠시 후 다시 시도해주세요."
+      />
+    )
+  }
   return (
     <div className={cx('container')}>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FullScreenMessage type="loading" />
+      {/* {JSON.stringify(wedding)}{' '} */}
     </div>
   )
 }
