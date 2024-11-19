85186de5c08c440a0ddac9ac4bfb0df00ee160aa
developer-jyyun <developer.jyyun@gmail.com>
2024-11-19 12:49:01 +0900

    Feat: 메인 컴포넌트 추가

---
+++ src/components/sections/Main.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames/bind'
+import Section from '@common/Section'
+import styles from './Main.module.scss'
+
+const cx = classNames.bind(styles)
+
+export default function Main() {
+  return (
+    <>
+      <Section className={cx('container')}>
+        <div className={cx('main')}>
+          <div className={cx('title-box')}>
+            <p>Welcome</p>
+            <p>to </p>
+            <p>Our</p>
+            <p>Wedding</p>
+          </div>
+          <img src="/assets/images/wd03.jpg" alt="메인이미지" />
+        </div>
+      </Section>
+    </>
+  )
+}
