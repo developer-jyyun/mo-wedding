b5549ad41a301e4cd0f56eab7eae25ef459a9e63
developer-jyyun <developer.jyyun@gmail.com>
2024-12-16 15:40:48 +0900

    Style: 캘린더 스타일 수정

--- src/components/sections/Calendar.tsx
+++ src/components/sections/Calendar.tsx
@@ -9,55 +9,7 @@ import styles from './Calendar.module.scss'
 
 const cx = classNames.bind(styles)
 //TODO:: 캘린더 반응형 및 디테일한 부분 스타일링
-const css = `
-.rdp-root{
-    width:100%;    
-    display: flex;
-    justify-content: center;
-}
-.rdp-months{   
-    justify-content:center;
-    padding:2rem;
-    background: rgba(255, 255, 255,.8);
-    border-radius:1rem;
-}
-.rdp-nav,.rdp-month_caption {
-    display: none;
-}
-.rdp-month_grid{
-    display:flex;
-    flex-direction: column;
-}
-.rdp-month_grid .rdp-weekday{
 
-    border-bottom:1px solid var(--muted-rose); font-weight:bold;
-}
-.rdp-weeks{
-    padding-top:2rem;
-}
-.rdp-day, .rdp-weekday{
-    color:var(--muted-brown);
-}
-.rdp-weekday, .rdp-day_button{
-    cursor:default;
-    width: 10vw;  
-    height: 10vw; 
-    line-height: 10vw; 
-    min-width: 40px;
-    min-height: 40px;
-    max-width: 80px;
-    max-height: 80px;
-}
-.rdp-selected{
-    background:var(--muted-rose);border-radius:50%; 
-    color:#fff;
-    font-weight:bold;
-}
-.rdp-selected .rdp-day_button{
-    border:none;
-}
-
-`
 interface Props {
   date: string
 }
@@ -88,3 +40,49 @@ export default function Calendar({ date }: Props) {
     </>
   )
 }
+const css = `
+.rdp-root{
+    width:100%;
+    height:100%;
+    display: flex;
+    justify-content: center;
+    background: rgba(255, 255, 255,.8);
+    border-radius:2rem;
+    padding:4rem;
+}
+
+.rdp-nav,.rdp-month_caption {
+    display: none;
+}
+.rdp-month_grid{
+    display:flex;
+    flex-direction: column;
+}
+.rdp-month_grid .rdp-weekday{
+
+    border-bottom:1px solid var(--muted-rose); font-weight:bold;
+}
+.rdp-weeks{
+    padding-top:2rem;
+}
+.rdp-day, .rdp-weekday{
+    color:var(--muted-brown);
+}
+.rdp-weekday, .rdp-day_button{
+    cursor:default; 
+    line-height: 1rem; 
+    min-width: 40px;
+    min-height: 40px;
+    max-width: 80px;
+    max-height: 80px;
+}
+.rdp-selected{
+    background:var(--muted-rose);border-radius:50%; 
+    color:#fff;
+    font-weight:bold;
+}
+.rdp-selected .rdp-day_button{
+    border:none;
+}
+
+`
